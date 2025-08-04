10521e0906dab5888a2ce85971c89c2d47255a87
robmolloy <robmolloy@hotmail.co.uk>
2025-08-04 15:58:44 +0100

    remove wrappers on DisplayMarkdown

--- src/modules/blog/DisplayMarkdown.tsx
+++ src/modules/blog/DisplayMarkdown.tsx
@@ -3,32 +3,30 @@ import Markdown from "react-markdown";
 
 export const DisplayMarkdown = (p: { children: string }) => {
   return (
-    <div className="flex justify-center">
-      <div className="react-markdown max-w-[700px]">
-        <Markdown
-          components={{
-            a: ({ children, href }) => {
-              if (!href) return <a>{children}</a>;
-              if (href?.startsWith("http"))
-                return (
-                  <a href={href} target="_blank" rel="noopener noreferrer">
-                    {children}
-                  </a>
-                );
+    <div className="react-markdown">
+      <Markdown
+        components={{
+          a: ({ children, href }) => {
+            if (!href) return <a>{children}</a>;
+            if (href?.startsWith("http"))
+              return (
+                <a href={href} target="_blank" rel="noopener noreferrer">
+                  {children}
+                </a>
+              );
 
-              return <Link href={href}>{children}</Link>;
-            },
-            img: ({ src, alt }) => (
-              <>
-                <img src={src} alt={alt} />
-                {alt && <div className="my-2 border-l pl-2 text-sm italic opacity-75">{alt}</div>}
-              </>
-            ),
-          }}
-        >
-          {p.children}
-        </Markdown>
-      </div>
+            return <Link href={href}>{children}</Link>;
+          },
+          img: ({ src, alt }) => (
+            <>
+              <img src={src} alt={alt} />
+              {alt && <div className="my-2 border-l pl-2 text-sm italic opacity-75">{alt}</div>}
+            </>
+          ),
+        }}
+      >
+        {p.children}
+      </Markdown>
     </div>
   );
 };
