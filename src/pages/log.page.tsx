5ed2921b924a43d4b23cd156b9093ee8b38d01ab
robmolloy <robmolloy@hotmail.co.uk>
2025-08-04 11:21:52 +0100

    add blog post stores to logpage

--- src/pages/log.page.tsx
+++ src/pages/log.page.tsx
@@ -1,3 +1,5 @@
+import { useBlogPostImageRecordsStore } from "@/modules/blog/blogPostImageRecordsStore";
+import { useBlogPostRecordsStore } from "@/modules/blog/blogPostRecordsStore";
 import { useUsersStore } from "@/modules/users/usersStore";
 import { useCurrentUserStore, useUnverifiedIsLoggedInStore } from "@/stores/authDataStore";
 
@@ -5,11 +7,23 @@ const LogPage = () => {
   const usersStore = useUsersStore();
   const currentUserStore = useCurrentUserStore();
   const unverifiedIsLoggedInStore = useUnverifiedIsLoggedInStore();
+  const blogPostRecordsStore = useBlogPostRecordsStore();
+  const blogPostImageRecordsStore = useBlogPostImageRecordsStore();
 
   return (
     <div>
       <pre>
-        {JSON.stringify({ usersStore, currentUserStore, unverifiedIsLoggedInStore }, undefined, 2)}
+        {JSON.stringify(
+          {
+            usersStore,
+            currentUserStore,
+            unverifiedIsLoggedInStore,
+            blogPostImageRecordsStore,
+            blogPostRecordsStore,
+          },
+          undefined,
+          2,
+        )}
       </pre>
     </div>
   );
