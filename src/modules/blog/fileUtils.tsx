e7866dc76a624eb5b7e65e5043163049dc3ffece
robmolloy <robmolloy@hotmail.co.uk>
2025-08-05 23:31:49 +0100

    add file utils

---
+++ src/modules/blog/fileUtils.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export const useFileUrl = (file: File | null | undefined) => {
+  const [url, setUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!file) return setUrl(null);
+
+    const objectUrl = URL.createObjectURL(file);
+    setUrl(objectUrl);
+
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [file]);
+
+  return url;
+};
