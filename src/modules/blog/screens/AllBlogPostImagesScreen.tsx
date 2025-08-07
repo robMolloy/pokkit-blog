a8844afd4decf7f0b5f89a6841adc202bdb5d812
robmolloy <robmolloy@hotmail.co.uk>
2025-08-07 15:38:10 +0100

    correctly set up AllBlogPostImagesScreen

--- src/modules/blog/screens/AllBlogPostImagesScreen.tsx
+++ src/modules/blog/screens/AllBlogPostImagesScreen.tsx
@@ -1,3 +1,35 @@
+import { MainLayout } from "@/components/layout/Layout";
+import { useBlogPostImageRecordsStore } from "../blogPostImageRecordsStore";
+import { H1 } from "@/components/ui/defaultComponents";
+import { BlogPostSummariesGrid } from "../BlogPostSummmariesGrid";
+import { BlogPostImageSummaryCard } from "../BlogPostImageSummaryCard";
+import { useRouter } from "next/router";
+
 export const AllBlogPostImagesScreen = () => {
-  return <div>AllBlogPostImagesScreen</div>;
+  const blogPostImageRecordsStore = useBlogPostImageRecordsStore();
+  const router = useRouter();
+
+  return (
+    <MainLayout>
+      <H1>All Blog Post Images</H1>
+      {(() => {
+        if (blogPostImageRecordsStore.data === null) return <>Error</>;
+        if (blogPostImageRecordsStore.data === undefined) return <>Loading...</>;
+        if (blogPostImageRecordsStore.data.length === 0) return <>No blog post images</>;
+
+        return (
+          <BlogPostSummariesGrid>
+            {blogPostImageRecordsStore.data.map((blogPostImageRecord) => (
+              <BlogPostImageSummaryCard
+                onClick={() => router.push(`/admin/blog-post-image/${blogPostImageRecord.id}`)}
+                key={blogPostImageRecord.id}
+                blogPostImageRecord={blogPostImageRecord}
+              />
+            ))}
+          </BlogPostSummariesGrid>
+        );
+        return;
+      })()}
+    </MainLayout>
+  );
 };
